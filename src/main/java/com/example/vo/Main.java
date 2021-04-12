37a5bd4b6590fe4d3946699b4b68d1c349fe6e3f
gaurav <gauravraj.it.rock@gmail.com>
2021-04-12 14:45:09 +0530

    commitng voapp test

---
+++ src/main/java/com/example/vo/Main.java
@@ -0,0 +1,36 @@
+package com.example.vo;
+
+import com.example.vo.Main.Id;
+
+@Entity
+@Id
+public class Main {
+	public @interface Id {
+
+	}
+
+	private Long id;
+	private String name;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
